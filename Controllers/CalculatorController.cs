68695b5c5cd09288442dd92ca181140d2896e3c2
Devnath03 <jmdevnath.c@gmail.com>
2025-02-01 19:44:58 +0530

    First Commit

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,77 @@
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace Calculator_Service.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class CalculatorController : ControllerBase
+//    {
+//    }
+//}
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Calculator_Service.Model;
+
+namespace Calculator_Service.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        [Route("api/calculate")]
+
+            public IHttpActionResult Add([FromBody] CalculationRequest model)
+            {
+                if (model == null)
+                    return BadRequest("Invalid Input");
+
+                double result = model.Number1 + model.Number1;
+                return Ok(new { Result = result });
+            }
+            [HttpPost]
+            [Route("subtract")]
+
+            public IHttpActionResult Subtract([FromBody] CalculatorModel model)
+            {
+                if (model == null)
+                    return BadRequest("Invalid Input");
+
+                double result = model.Number1 - model.Number1;
+                return Ok(new { Result = result });
+            }
+
+            [HttpPost]
+            [Route("multiply")]
+
+            public IHttpActionResult Multiply([FromBody] CalculatorModel model)
+            {
+                if (model == null)
+                    return BadRequest("Invalid Input");
+
+                double result = model.Number1 * model.Number1;
+                return Ok(new { Result = result });
+            }
+
+            [HttpPost]
+            [Route("division")]
+            public IHttpActionResult Division([FromBody] CalculatorModel model)
+            {
+                if (model == null)
+                    return BadRequest("Invalid Input");
+
+                if (model.Number1 == 0 || model.Number2 == 0)
+                    return BadRequest("zero is not allowed");
+
+                double result = model.Number1 / model.Number2;
+                return Ok(new { Result = result });
+            }
+
+        }
+    }
+
+
---
+++ Model/CalculationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator_Service.Model
+{
+    public class CalculationRequest
+    {
+
+        public double Operand1 { get; set; }
+        public double Operand2 { get; set; }
+        public string Operator { get; set; } // "+", "-", "*", "/"
+
+    }
+}
